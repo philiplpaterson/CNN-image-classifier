5afcb73062a0218b695111ead40c3ce05a77dffa
Philip Paterson <philiplpaterson@gmail.com>
2024-03-28 23:16:16 -0400

    Finished model

---
+++ cnn_image_classifier.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+import pandas as pd
+import pickle
+import random
+import math
+import os
+from sklearn import metrics
+
